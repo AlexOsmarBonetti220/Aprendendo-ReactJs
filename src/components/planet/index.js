b5ee7ded7107d13cf3338383c8a313f851724a34
alex-bonetti <62677325+alex-bonetti@users.noreply.github.com>
2020-03-28 14:55:59 -0300

    Passando eventos por parâmetro

--- src/components/planet/index.js
+++ src/components/planet/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Img from "../shared/index";
 import Description from "../shared/description";
+import Botao from "../shared/botao";
 
 export default class Planet extends React.Component {
     render(){
@@ -12,7 +13,8 @@ export default class Planet extends React.Component {
                         <h4>{this.props.nome}</h4>
                         <Img img={this.props.url} />
                         <Description desc={this.props.desc} link={this.props.link} />
-                        
+                        <br></br>
+                        <Botao clique={() => this.props.clickNoPlaneta(this.props.nome)} />
                     </li>
                 </ul>
             </div>
